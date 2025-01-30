e11296b24984e48852ca2998de4c03d690ce0c1c
kaniela04 <karenguzmanhernandez44@gmail.com>
2025-01-29 20:50:31 -0500

    creacion de interfaz , viculacion de propiedades + interpolacion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [HousingLocationComponent],
@@ -7,5 +8,16 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://img.freepik.com/foto-gratis/casa-aislada-campo_1303-23773.jpg';
 
+  housinglocation: Housinglocation = {
+    id: 99990,
+    name: 'House-Cottage',
+    city: 'texas',
+    state: 'ST',
+    photo: this.baseUrl, // Corregido
+    availableUnits: 10000000,
+    wifi: true,
+    laundry: false,
+  };
 }
